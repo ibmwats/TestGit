8035ca007ad2d29953803d38e15fc2e0dd3beb08
ibmwats <ibm.watson@mail.ru>
2022-12-24 11:09:40 +0300

    Проверка второй версии!

--- main.py
+++ main.py
@@ -1,9 +1,7 @@
-import os
-
-print('Test Git')
-
-
-print(f'Тестим Русский язык в передаче!')
-
-
-print(f'Тестируем всякое разное...')
+import psutil # pip install psutil
+proc_name = 'Google Chrome.exe'
+for proc in psutil.process_iter():
+    if proc.name() == proc_name:
+        print("Process {}  started".format(proc_name))
+    else:
+        print("Процесс запущен!")
